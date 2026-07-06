df926e449f5931664dcbe3744cde556b03a780e4
marioma-samir <mariam.samir20033@gmail.com>
2026-07-07 01:55:36 +0300

    Fix merge conflict

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
 window.addEventListener("load", function () {
 
-    setTimeout(function(){
+    setTimeout(function () {
 
         document.getElementById("loader").style.opacity = "0";
 
-        setTimeout(function(){
+        setTimeout(function () {
 
             document.getElementById("loader").style.display = "none";
 
-        },1000);
+        }, 1000);
 
-    },2000);
+    }, 2000);
 
-=======
-window.addEventListener("load", function () {
-
-    setTimeout(function(){
-
-        document.getElementById("loader").style.opacity = "0";
-
-        setTimeout(function(){
-
-            document.getElementById("loader").style.display = "none";
-
-        },1000);
-
-    },2000);
-
->>>>>>> e5cabc3e6ae40ecacc5009987f10fe1bf5d2759b
 });
